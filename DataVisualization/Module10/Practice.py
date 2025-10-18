413b129f81760bc2252414ff5c5ceb8eb1986167
elfaradio <farhadulislam84054@gmail.com>
2025-10-18 23:21:11 +0600

    del

--- DataVisualization/Module10/Practice.py
+++ DataVisualization/Module10/Practice.py
@@ -1,1 +1,6 @@
 '''1. You have a list of temperatures recorded in Celsius. Your task is to: Create a NumPy array from the given list. Convert all temperatures from Celsius to Fahrenheit. The formula is F = C* 1.8 + 32. Print the new Fahrenheit array.'''
+import numpy as np
+a = np.random.randint(10, 100, 10)
+print(a)
+a = a*1.8+32
+print(a)
