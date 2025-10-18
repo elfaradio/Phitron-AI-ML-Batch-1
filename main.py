d69f537133a295a8b55bf95edae7c3d520a7c133
elfaradio <farhadulislam84054@gmail.com>
2025-10-18 21:53:48 +0600

    assignment

--- main.py
+++ main.py
@@ -1,27 +1,4 @@
-# Getter and Setter MEthods
-class Person:
-    def __init__(self, name, age, balance):
-        self.name = name  # public variables
-        self._age = age  # protected variables -> Child class access
-        self.__balance = balance  # private -> only nijer class
+s = input().split()
 
-    def info(self):
-        print("Balance ", self.__balance)
-
-    def get_balance(self):
-        return self.__balance
-
-    def set_balance(self, money):
-        self.__balance += money
-
-# bayrer method
-
-
-def get_name(person):
-    return person.name
-
-
-person1 = Person("Rakib", 20, 10000)
-person1.set_balance(500)
-print(person1.name)
-print(get_name(person1))
+ans = max(s, key=lambda x: len(x) < 3)
+print(ans)
