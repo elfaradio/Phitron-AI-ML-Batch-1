a3f6461bdb13746d5ef290875c02d98297d7c2db
elfaradio <farhadulislam84054@gmail.com>
2025-10-23 20:47:15 +0600

    done

--- main.py
+++
@@ -1,3 +0,0 @@
-# Importing the NumPy library with an alias 'np'
-import numpy as np
-print(np.random.choice(7, 5))
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,13 +1,12 @@
-s = input()
-# print(s
-# )
+# Example usage
+customers = pd.DataFrame({
+    'id': [1, 2, 3, 4],
+    'name': ['Joe', 'Henry', 'Sam', 'Max']
+})
 
-mp = {}
+orders = pd.DataFrame({
+    'id': [1, 2],
+    'customerId': [3, 1]
+})
 
-for i in s:
-    mp[i] = mp.get(i, 0)+1
-
-
-# print(mp)
-for i in mp:
-    print(i, " :", mp[i])
+print(find_customers(customers, orders))
